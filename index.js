b4de25fde5c5a5cbbb21578e26e9bc9b82c33b69
Abbas <abbas.mansoori@iths.se>
2021-04-22 01:02:00 +0200

    lagt till och testat mvc

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const dotenv = require("dotenv").config()
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-
+const logger = require("./middleware/logger")
 app.use(express.json())
 
 const PORT = process.env.PORT || 8080 || 3000
 
+//testa middleware
 
+app.use(logger)
 
-
-
+app.use("/api/users", require("./routes/users"))
 
 
 
---
+++ migrations/users.js
@@ -0,0 +1,17 @@
+const users = [
+    {
+        email: "stabbing.steve@fuskeluring.hack",
+        password: "grillkorv123"
+    },
+    {
+        email: "murdering.mike@fuskeluring.hack",
+        password: "bananpaj1337"
+    },
+    {
+       email: "crimes.johnsson@fuskeluring.hack", 
+       password: "sötsursås42"
+
+    }
+];
+
+module.exports = users;
