ead89d7e86bf6214676107b826aa8b82a955c350
AbbasMansoori <abbas.mansoori@iths.se>
2021-04-21 19:06:33 +0200

    commit

---
+++ database/database.js
@@ -0,0 +1,22 @@
+const sqlite = require("sqlite3")
+const db = new sqlite.Database("database/fuskeluring.db")
+
+db.serialize(() => {
+    db.run("DROP TABLE IF EXISTS users")
+    db.run("DROP TABLE IF EXISTS fakes")
+    db.run(`CREATE TABLE "users" ("userId" INTEGER, "email" TEXT NOT NULL, "password" TEXT NOT NULL, PRIMARY KEY ("userId" AUTOINCREMENT));`)
+    db.run(`CREATE TABLE "fakes" ("fakeId" INTEGER, "belongsToUser" INTEGER, "adress" TEXT NOT NULL, "city" TEXT NOT NULL, "DOB" INTEGER, "fun_fact" TEXT NOT NULL, "picture" TEXT NOT NULL, "occupation" TEXT NOT NULL, PRIMARY KEY ("fakeID" AUTOINCREMENT), FOREIGN KEY ("belongsToUser") REFERENCES "users"(userId));`)
+    db.get("PRAGMA foreign_keys = ON")
+})
+
+
+
+
+
+
+
+
+
+
+
+module.exports = db
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config()
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const sqlite = require("sqlite3")
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 8080 || 3000
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function () {
+    console.log(`Server started at ${PORT}`);
+  });
+  
+  
