ff5e81dc965131559980cbf61b9ecf8f352cfc6f
AbbasMansoori <abbas.mansoori@iths.se>
2021-04-22 16:58:49 +0200

    user seed created with function

--- database/database.js
+++ database/database.js
@@ -1,5 +1,6 @@
 const sqlite = require("sqlite3")
 const db = new sqlite.Database("database/fuskeluring.db")
+const initSeed = require("../database/seed")
 
 db.serialize(() => {
     db.run("DROP TABLE IF EXISTS users")
@@ -7,6 +8,7 @@ db.serialize(() => {
     db.run(`CREATE TABLE "users" ("userId" INTEGER, "email" TEXT NOT NULL, "password" TEXT NOT NULL, PRIMARY KEY ("userId" AUTOINCREMENT));`)
     db.run(`CREATE TABLE "fakes" ("fakeId" INTEGER, "belongsToUser" INTEGER, "adress" TEXT NOT NULL, "city" TEXT NOT NULL, "DOB" INTEGER, "fun_fact" TEXT NOT NULL, "picture" TEXT NOT NULL, "occupation" TEXT NOT NULL, PRIMARY KEY ("fakeID" AUTOINCREMENT), FOREIGN KEY ("belongsToUser") REFERENCES "users"(userId));`)
     db.get("PRAGMA foreign_keys = ON")
+    initSeed(db)
 })
 
 
--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const users = require("../migrations/users")
+
 
 
 // testa våran routes och att få tillbaka users från usersfilen
