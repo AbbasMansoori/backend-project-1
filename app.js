0b6eeb67595f7818cb53e9163955dbda3463f544
AbbasMansoori <abbas.mansoori@iths.se>
2021-04-24 10:23:55 +0200

    boilerplate

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv").config()
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
-const logger = require("./middleware/logger")
+
 app.use(express.json())
 
 const PORT = process.env.PORT || 8080 || 3000
